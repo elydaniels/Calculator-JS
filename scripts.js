186e16bf98415cb4622f1be3cf5cab3fd77f0a97
Elizabeth Daniels <elydaniels81@gmail.com>
2022-07-31 18:54:06 -0400

    updating

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 //Get string on display with numbers and operator
 let getDisplay = document.getElementById('result')
 const insert = num => {
@@ -26,26 +24,3 @@ getDisplay.innerHTML = stringMath(result)
 getDisplay.innerHTML = 'Nothing...'
   }
 }
-=======
-let getEl = document.getElementById('result')
-const insert = num => {
-  const number = getEl.innerHTML
-  getEl.innerHTML = number + num
-}
-
-const clean = () => (getEl.innerHTML = '')
-
-const percent = () => {
-  const numb = getEl.innerHTML
-  getEl.innerHTML = numb * 0.01
-}
-
-const calculator = () => {
-  const result = getEl.innerHTML
-  if (result) {
-    getEl.innerHTML = eval(result)
-  } else {
-    getEl.innerHTML = 'Nothing...'
-  }
-}
->>>>>>> 315f0398c60aca31532ad3f5dbf4c293e345964e
